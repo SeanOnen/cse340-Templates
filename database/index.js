8921e16a3db9e17529a63e75d0d12638b05e650d
SeanOnen <seanthomasongayaonen@gmail.com>
2026-01-24 21:07:09 +0300

    index.js edit

--- database/index.js
+++ database/index.js
@@ -3,22 +3,18 @@ require("dotenv").config()
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
-// Optional query logger for dev
 module.exports = {
   async query(text, params) {
     try {
       const res = await pool.query(text, params)
-      if (process.env.NODE_ENV !== "production") {
-        console.log("executed query", { text })
-      }
       return res
     } catch (error) {
-      console.error("error in query", { text })
+      console.error("Database query error", error)
       throw error
     }
   },
